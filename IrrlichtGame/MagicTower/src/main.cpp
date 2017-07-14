4ae331fc9d744c517b5e8fbbe93ea9b676e7ae00
tacthgin <821910196@qq.com>
2017-07-14 17:36:29 +0800

    创建一个简单的窗口

--- IrrlichtGame/MagicTower/src/main.cpp
+++ IrrlichtGame/MagicTower/src/main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
+#include <irrlicht.h>
+
+using namespace irr;
+using namespace core;
+using namespace video;
+using namespace scene;
 
 int main()
 {
+	IrrlichtDevice *device = createDevice(video::EDT_SOFTWARE, dimension2d<u32>(640, 480), 16,
+			false, false, false, 0);
+	if (!device)
+		return 1;
+	device->setWindowCaption(L"Hello Irrlicht!");
+	IVideoDriver* driver = device->getVideoDriver();
+	ISceneManager* smgr = device->getSceneManager();
+
+	while (device->run())
+	{
+		driver->beginScene(true, true, SColor(255, 25, 25, 25));
+		smgr->drawAll();
+		driver->endScene();
+	}
+
+	device->drop();
 	return 0;
 }
